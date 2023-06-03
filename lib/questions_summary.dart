475253321913013e30e564f4db921a924a892c33
Varun Gotmare <64700310+VarunGotmare@users.noreply.github.com>
2023-06-03 09:23:14 +0530

    updated with decorations

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -15,52 +15,59 @@ class QuestionsSummary extends StatelessWidget {
             if (data['user_ans'] == data['correct_ans']) {
               colorAns = const Color.fromARGB(255, 141, 245, 144);
             }
-            return Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  child: CircleAvatar(
-                    radius: 20,
-                    backgroundColor: colorAns,
-                    child:
-                        Text(((data['question_index'] as int) + 1).toString()),
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 8,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(7),
+                    child: CircleAvatar(
+                      radius: 15,
+                      backgroundColor: colorAns,
+                      child: Text(
+                          ((data['question_index'] as int) + 1).toString()),
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        data['questions'] as String,
-                        style: GoogleFonts.lato(
-                          fontSize: 14,
-                          color: Colors.white,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data['questions'] as String,
+                          style: GoogleFonts.lato(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.start,
                         ),
-                        textAlign: TextAlign.start,
-                      ),
-                      const SizedBox(height: 7),
-                      Text(
-                        data['user_ans'] as String,
-                        style: GoogleFonts.lato(
-                          fontSize: 14,
-                          color: Colors.white,
+                        const SizedBox(height: 7),
+                        Text(
+                          ('Your Answer : ') + (data['user_ans'] as String),
+                          style: GoogleFonts.lato(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.start,
                         ),
-                        textAlign: TextAlign.start,
-                      ),
-                      const SizedBox(height: 7),
-                      Text(
-                        data['correct_ans'] as String,
-                        style: GoogleFonts.lato(
-                          fontSize: 14,
-                          color: Colors.white,
+                        const SizedBox(height: 7),
+                        Text(
+                          ('Correct Answer : ') +
+                              (data['correct_ans'] as String),
+                          style: GoogleFonts.lato(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.start,
                         ),
-                        textAlign: TextAlign.start,
-                      ),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
-                )
-              ],
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             );
           }).toList(),
         ),
